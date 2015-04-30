161e839f844c8e3a2f5c039a390ad5ae3b0e89c7
Chadwick Flores <chadwickflores@gmail.com>
2015-04-29 22:52:50 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+feeds = Feed.create([
+	{ title: 'BBC World', url: 'http://feeds.bbci.co.uk/news/world/rss.xml', status: 'active', category: 'news' }, 
+ 	{ title: 'NY Times', url: 'http://rss.nytimes.com/services/xml/rss/nyt/World.xml', status: 'active', category: 'news' }
+	])
